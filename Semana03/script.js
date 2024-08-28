9d2e5fcd6ed339eca0c75f2ada14eff8ce1a0641
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-08-28 17:25:34 -0300

    semana3

---
+++ Semana03/script.js
@@ -0,0 +1,36 @@
+
+let users = [];
+document.getElementById('userForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Captura os valores do formulário
+    let name = document.getElementById('name').value;
+    let idade = document.getElementById('idade').value;
+    let corFav = document.getElementById('corFav').value;
+
+    // Adiciona o novo usuário ao array
+    users.push({ name, idade, corFav });
+
+    // Ordena o array por nome
+    users.sort((a, b) => a.name.localeCompare(b.name));
+
+    // Limpa os campos do formulário
+    document.getElementById('userForm').reset();
+});
+
+document.getElementById('viewListButton').addEventListener('click', function () {
+    // Atualiza a lista no HTML
+    updateUserList();
+})
+
+
+function updateUserList() {
+    let userList = document.getElementById('userList');
+    userList.innerHTML = ''; // Limpa a lista
+
+    users.forEach(user => {
+        let li = document.createElement('li');
+        li.textContent = `Nome: ${user.name}, Idade: ${user.age}, Cor Preferida: ${user.favoriteColor}`;
+        userList.appendChild(li);
+    });
+}
